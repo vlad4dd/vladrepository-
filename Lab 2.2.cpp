c5a1c5dd0b922d02cc6da5cff8bdfc16692e9410
vlad4dd <vladi4ep@gmail.com>
2023-11-08 22:40:23 +0300

    new file

---
+++ Lab 2.2.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+
+int main() 
+ 
+{
+	int a, b, c;
+	float x;
+	cout << "input a:";
+	cin >> a;
+	cout << "input b:";
+	cin >> b;
+	cout << "input c:";
+	cin >> c;
+	x = (float)(a - b)/(c + 1); 
+	cout << "x=" << fixed << setprecision(2) << x << endl; 
+	system("pause");
+	return 0;
+	
+}
+
+
