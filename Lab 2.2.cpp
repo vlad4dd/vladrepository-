5147e2bea506014a7b65ccc63b2d31f0b4b81c69
vlad4dd <vladi4ep@gmail.com>
2023-11-29 16:38:02 +0300

    changes made

--- Lab 2.2.cpp
+++ Lab 2.2.cpp
@@ -8,15 +8,15 @@ using namespace std;
 int main() 
  
 {
-	int a, b, c;
+	int a, b, d;
 	float x;
 	cout << "input a:";
 	cin >> a;
 	cout << "input b:";
 	cin >> b;
-	cout << "input c:";
-	cin >> c;
-	x = (float)(a - b)/(c + 1); 
+	cout << "input d:";
+	cin >> d;
+	x = (float)(a - b)/(d + 1); 
 	cout << "x=" << fixed << setprecision(2) << x << endl; 
 	system("pause");
 	return 0;
